d188bdeb120c6e54c45b5fe17ad9f80e9cb655be
ruslanvoloshchenko <ruslanvoloshchenko@outlook.com>
2024-05-26 19:19:57 -0700

    Change output directory settings.

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin-one-vue-tailwind/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    outDir: '../Haraka/http/html'
+  }
 });
